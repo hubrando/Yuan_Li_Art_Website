eb21d3c4a8d28a7c25b5b476d0337ddb26266f55
Brandon Hu <71099951+hubrando@users.noreply.github.com>
2023-05-09 18:10:14 -0400

    Trying to connect everything together.

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ErrorPage from "./error-page"
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Root from "./routes/root";
+import Contact from "./routes/contacts";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
-  },
+    element: "",
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
