da36c7ac5d65b25eb282cf3fcf87317750289648
Brandon Hu <71099951+hubrando@users.noreply.github.com>
2023-05-17 12:16:44 -0400

    Add CSS formatting.

--- my-app/src/components/ContactPage.tsx
+++ my-app/src/components/ContactPage.tsx
@@ -1,5 +1,7 @@
+import './ContactPage.css'
+
 function Contact() {
-    return <div>
+    return <div className='page-information'>
         <h1>Contact Yuan Li</h1>
         <p>You can reach Yuan Li via email at yuanli2009@gmail.com.</p>
     </div>
